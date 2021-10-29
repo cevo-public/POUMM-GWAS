5a4d73fb13c50dab18f9a512f12559370c8d7de7
Sarah Nadeau <nadeaus@bs-mbpr509.d.ethz.ch>
2021-10-29 10:28:26 +0200

    add scripts to generate container to do GWAS, don't have -9 phenotype for any samples

--- scripts/R/make_gwas_phenotypes_file.R
+++ scripts/R/make_gwas_phenotypes_file.R
@@ -5,12 +5,11 @@ require(tidyr)
 require(dplyr)
 
 OUTDIR <- "output"
-DATADIR <- "/Volumes/stadler/SHCSData/data"
-FAM_FN <- "spVL.shcs.rs_with_sex_SN.fam"
+FAM_FN <- "spVL.shcs.rs.filtered.fam"
 
 # Load files
 print("Loading fam and corrected trait data.")
-fam <- read.delim(file =  paste(DATADIR, FAM_FN, sep = "/"), sep = "", header = F)
+fam <- read.delim(file =  paste(OUTDIR, FAM_FN, sep = "/"), sep = "", header = F)
 colnames(fam) <- c("FID", "IID", "IID_father", "IID_mother", "sex", "phenotype")  # https://www.cog-genomics.org/plink/1.9/formats#fam
 
 corrected_trait <- read.csv(file = paste(OUTDIR, "poumm_corrected_traits.csv", sep = "/"))
@@ -20,9 +19,10 @@ phenotypes <- merge(
   fam, corrected_trait %>% select(id, h.MWA, trait),
   by.x = "IID", by.y = "id",
   all.x = T, all.y = F)  # may have used more individuals for POUMM fitting than have genotype data
-phenotypes <- phenotypes %>%
-  replace_na(replace = list("h.MWA" = -9, "trait" = -9)) %>%
-  select(FID, IID, h.MWA, trait)
+if(any(is.na(phenotypes$h.MWA) | is.na(phenotypes$trait))) {
+  stop("Some genomes after filtering don't have a phenotype!")
+}
+phenotypes <- phenotypes %>% select(FID, IID, h.MWA, trait)
 
 write.table(
   x = phenotypes,
---
+++ scripts/bash/run-gwas.sh
@@ -0,0 +1,68 @@
+# This script runs GWAS with both phenotypes, sex and top 5 PCs of genetic variation as co-variates.
+# Run as: bash scripts/bash/run-gwas.sh
+
+DATA_PREFIX="spVL.shcs.rs.filtered"
+MEM=10000
+
+# Get variants in LD to exclude from PCA calculation
+./plink2 \
+--bfile output/$DATA_PREFIX \
+--indep-pairwise 50 5 0.2 \
+--memory $MEM \
+--out output/$DATA_PREFIX
+
+# Get top 5 PCs for covariates based on SNPs in linkage equilibrium
+./plink2 \
+--bfile output/$DATA_PREFIX \
+--exclude output/${DATA_PREFIX}.prune.out \
+--pca \
+--out output/$DATA_PREFIX \
+--memory $MEM
+awk '{print $1,$2,$3,$4,$5,$6,$7}' output/$DATA_PREFIX.eigenvec >  output/${DATA_PREFIX}.pc5.txt
+
+# Run GWAS
+./plink2 \
+--bfile output/$DATA_PREFIX \
+--pheno output/gwas_phenotypes.txt \
+--glm sex \
+--covar output/spVL.shcs.rs.filtered.pc5.txt \
+--memory $MEM \
+--out output/$DATA_PREFIX
+
+# Extract SNP-only results, without covariates
+head -1 output/${DATA_PREFIX}.trait.glm.linear > output/${DATA_PREFIX}.trait.glm.linear.nocovariates
+grep -h 'ADD' output/${DATA_PREFIX}.trait.glm.linear >> output/${DATA_PREFIX}.trait.glm.linear.nocovariates
+
+head -1 output/${DATA_PREFIX}.h.MWA.glm.linear > output/${DATA_PREFIX}.h.MWA.glm.linear.nocovariates
+grep -h 'ADD' output/${DATA_PREFIX}.h.MWA.glm.linear >> output/${DATA_PREFIX}.h.MWA.glm.linear.nocovariates
+
+# Paste results from both trait values together
+paste "output/${DATA_PREFIX}.trait.glm.linear.nocovariates" "output/${DATA_PREFIX}.h.MWA.glm.linear.nocovariates" | awk '{print $1, $2, $3, $9, $12, $22, $25}' > output/gwas_results.txt
+HEADER="CHROM POS ID BETA_standard P_standard BETA_corrected P_corrected"
+sed -i.bak "1 s/^.*$/$HEADER/" output/gwas_results.txt
+
+# Add p-value column to results
+awk '
+function abs(v) { return v < 0 ? -v : v }
+BEGIN { OFS = " " }
+NR == 1 {
+$8 = "neg_log10_P_standard";
+$9 = "neg_log10_P_corrected";
+$10 = "neg_log10_P_standard_minus_corrected";
+$11 = "abs_neg_log10_P_standard_minus_corrected" }
+NR >= 2 {
+$8 = -log($5)/log(10);
+$9 = -log($7)/log(10);
+$10 = $8 - $9
+$11 = abs($10)} 1' < output/gwas_results.txt > output/gwas_results.pvals.txt
+
+# Add beta (effect size) column to results
+awk '
+function abs(v) { return v < 0 ? -v : v }
+BEGIN { OFS = " " }
+NR == 1 {
+$12 = "abs_BETA_standard_minus_corrected" }
+NR >= 2 {
+$12 = abs($4 - $6) } 1' < output/gwas_results.pvals.txt > output/gwas_results.pvals.effectsize.txt
+
+
