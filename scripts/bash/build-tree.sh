5b520915927ad9c7f462280db2bc3a905c83e909
Sarah Nadeau <nadeaus@bs-mbpr342.d.ethz.ch>
2022-05-30 15:21:37 +0200

    output all locally optimal trees to account for phylo uncertainty

--- scripts/bash/build-tree.sh
+++ scripts/bash/build-tree.sh
@@ -13,10 +13,12 @@
 sed -e 's/\(^>.*$\)/#\1#/' output/pathogen.fasta | tr -d "\r" | tr -d "\n" | sed -e 's/$/#/' | tr "#" "\n" | sed -e '/^$/d' > output/pathogen_oneline.fasta
 cut -c 1-1505 output/pathogen_oneline.fasta > output/pathogen_trimmed.fasta
 
-# Build a ML tree with the GTR+F+R4 model and (TODO) 100 bootstrap replicates
+# Build a ML tree with the GTR+F+R4 model
+# -wt writes out all locally optimal trees
 iqtree \
 -s output/pathogen_trimmed.fasta \
 -m GTR+F+R4 \
+-wt \
 -redo \
 -pre output/pathogen \
 -nt AUTO
@@ -25,6 +27,3 @@ iqtree \
 # 38 reference pol sequences subtypes A-K from Los Alamos HIV database used as outgroup
 # Used RAxML to build the tree, RAxML default model is hard to tell from quick look at documentation
 # Used 100 bootstrap replicates
-
-
-
